868c86c39ce3cc1b5a67bf680e0eb2e0e35eef9c
sandra <sanesga@gmail.com>
2019-11-02 15:12:24 +0100

    Añadido: Casi finalizado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////
 //elementos
 var nivelElement = document.getElementById("nivel"); //span del nivel
-var boton = document.getElementById("boton");
+var start = document.getElementById("start");
 var rojo = document.getElementById("rojo");
 var verde = document.getElementById("verde");
 var amarillo = document.getElementById("amarillo");
@@ -18,9 +18,12 @@ var contador =0;
 var posicion = new Number();
 let i = new Number();
 var total=0;
+var porcentaje=0;
+var primero=false;
+
 
 //listeners
-boton.addEventListener("click", juegaMaquina); //al hacer click sobre el botón iniciamos el juego
+start.addEventListener("click", juegaMaquina); //al hacer click sobre el botón iniciamos el juego
 rojo.addEventListener("click", juegaUsuario);
 azul.addEventListener("click", juegaUsuario);
 verde.addEventListener("click", juegaUsuario);
@@ -28,11 +31,10 @@ amarillo.addEventListener("click", juegaUsuario);
 /////////////////////////////////////////////////////////
 
 
-
-
 //************************************MÁQUINA**************************************
 
 function juegaMaquina() {
+ 
 mensajes.innerHTML="Turno de la máquina";  //mientras juega la máquina, deshabilitamos todos los clicks de los colores
   rojo.style.pointerEvents = "none";
   azul.style.pointerEvents = "none";
@@ -46,41 +48,48 @@ mensajes.innerHTML="Turno de la máquina";  //mientras juega la máquina, deshab
   patron.push(colores[posicion]);
   console.log("patron " + patron);
 
+
   for (i = 0; i < patron.length; i++) {
+
     (function(i) {
-      setTimeout(function() {
+      if(!primero){
         colorear(patron[i]);
-        console.log("esperamos 1500 ms");
-      }, 1500 * (i + 1));
+        primero=true;
+      }else{
+        setTimeout(function() {
+          colorear(patron[i]);
+          console.log("esperamos 1500 ms");
+        }, 1500 * (i + 1));
+      }
     })(i);
   }
-  //if (i == patron.length) {
-    //console.log(i);
+ 
     console.log(patron.length);
+
+   
     setTimeout(function() {
       mensajes.innerHTML = "Tu turno";
       rojo.style.pointerEvents = "auto";
       azul.style.pointerEvents = "auto";
       amarillo.style.pointerEvents = "auto";
       verde.style.pointerEvents = "auto";
-    }, 2200 * patron.length);
-  //}
+    }, (2350 * patron.length)-(((2350*patron.length)*20)/100));
+
 }
 
 //*************************************JUGADOR**************************************
 
 function juegaUsuario() {
-//console.log("usuario " + this.id);
-// console.log("maquina " + patron[contador]);
-// console.log("length de patron " + patron.length);
 
   borrarMensajes();
   if (this.id == patron[contador]) {
-    colorear(this.id);
+    click(this.id);
     contador++;
     if(contador==patron.length){
       subirNivel();
-      mensajes.innerHTML="Patrón correcto. Subimos al nivel " + nivel;
+     // setTimeout(function(){
+       // mensajes.innerHTML="Patrón correcto. Subimos al nivel " + nivel;
+     // }, 2000);
       contador=0;
       juegaMaquina();
     }
@@ -94,16 +103,17 @@ function finJuego() {
   setTimeout(function(){
     habilitarbBotonStart();
     borrarMensajes();
+    borrarErrores();
     patron=[];
     nivel=0;
     contador=0;
   },2000);
 }
 function deshabilitarBotonStart() {
-  boton.style.display = "none";
+  start.style.display = "none";
 }
 function habilitarbBotonStart() {
-  boton.style.display = "inline-block";
+  start.style.display = "inline-block";
 }
 function subirNivel() {
   nivel++;
@@ -114,14 +124,20 @@ function generarNumeroAleatorio() {
 function borrarMensajes(){
   mensajes.innerHTML="";
 }
+function borrarErrores(){
+  errores.innerHTML="";
+}
+
 function colorear(elemento) {
   switch (elemento) {
     case "rojo":
       //console.log("colorear rojo");
       rojo.style.backgroundColor = "#FF5900";
+      rojo.style.transform="scale(1.3)";
       setTimeout(function() {
         console.log("esperamos 700 ms");
         rojo.style.backgroundColor = "#B24207";
+        rojo.style.transform="scale(1)";
       }, 700);
       break;
     case "azul":
@@ -150,6 +166,45 @@ function colorear(elemento) {
 
         amarillo.style.backgroundColor = "#B5A509";
       }, 700);
+      break;
+  }
+}
+function click(elemento) {
+  switch (elemento) {
+    case "rojo":
+      //console.log("colorear rojo");
+      rojo.style.backgroundColor = "#FF5900";
+      setTimeout(function() {
+        console.log("esperamos 150 ms");
+        rojo.style.backgroundColor = "#B24207";
+      }, 150);
+      break;
+    case "azul":
+      //console.log("colorear azul");
+      azul.style.backgroundColor = "#6FDEFF";
+      setTimeout(function() {
+        console.log("esperamos 150 ms");
+
+        azul.style.backgroundColor = "#0D589F";
+      }, 150);
+      break;
+    case "verde":
+      //console.log("colorear verde");
+      verde.style.backgroundColor = "#7DFF28";
+      setTimeout(function() {
+        console.log("esperamos 150 ms");
+
+        verde.style.backgroundColor = "#208709";
+      }, 150);
+      break;
+    case "amarillo":
+      //console.log("colorear amarillo");
+      amarillo.style.backgroundColor = "#FFF520";
+      setTimeout(function() {
+        console.log("esperamos 150 ms");
+
+        amarillo.style.backgroundColor = "#B5A509";
+      }, 150);
       break;
   }
 }
